a19d58d47f2318be770f13d9410fe3a56dbe286f
Seiya <seiya8bit@outlook.com>
2025-12-11 06:43:36 +0900

    chore: display my and dummy profiles

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,13 @@ export default async function Home() {
   const supabase = await createClient()
   const { data: { user } } = await supabase.auth.getUser()
 
-  const profile = await fetchServer(`/profiles/${user?.id}`)
+  const myProfile = await fetchServer(`/profiles/${user?.id}`)
+  const othersProfile = await fetchServer('/profiles/2b5e90e5-e35c-4031-8bb0-898d2b1402d8')
 
   return (
-    <>
-      Profile: {JSON.stringify(profile)}
-    </>
+    <div className="flex flex-col gap-4">
+      <span>My Profile: {JSON.stringify(myProfile)}</span>
+      <span>Dummy Profile: {JSON.stringify(othersProfile)}</span>
+    </div>
   );
 }
